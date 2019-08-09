302d42bb891a0b585737ddb3152277077c8381e9
Maciej Piotrowski <paciej00@github.com>
2019-08-09 13:42:18 +0200

    Allows toggling between Jumbo Index and normal Playing Card

--- PlayingCardsShowcaseApp/ContentView.swift
+++ PlayingCardsShowcaseApp/ContentView.swift
@@ -5,14 +5,27 @@ import PlayingCardsUI
 struct ContentView : View {
     var cards: [Card] = Card.createCards(52)
     var card: Card { return cards.first! }
-
+    @State var playingCardView: Bool = true
     var body: some View {
-        List(cards) { card in
-            HStack {
-                Spacer()
-                PlayingCardView(card: card)
-                Spacer()
+        NavigationView {
+            List(cards) { card in
+                Section {
+                    HStack {
+                        Spacer()
+                        if(self.playingCardView) {
+                            PlayingCardView(card: card)
+                        } else {
+                            JumboIndexPlayingCardView(card: card)
+                        }
+                        Spacer()
+                    }
+                }.scaleEffect(0.8, anchor: .center)
             }
+            .navigationBarTitle(Text("Playing Cards"))
+            .navigationBarItems(trailing:
+                Button(action: { self.playingCardView.toggle() }) {
+                    Image(systemName: "arrow.2.circlepath")
+            })
         }
     }
 }
